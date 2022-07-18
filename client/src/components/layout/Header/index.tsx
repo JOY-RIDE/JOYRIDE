344762a73236ee93d6a02187eb6bb194f44902f4
Suehyun Kim <sue555@naver.com>
2022-07-19 00:37:08 +0900

    design: mobile dropdown navigation & search redirect

--- client/src/components/layout/Header/index.tsx
+++ client/src/components/layout/Header/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import Container from 'components/common/Container';
 import logo from 'assets/images/logo.svg';
@@ -8,12 +8,32 @@ import { FaRegUserCircle } from 'react-icons/fa';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { useForm } from 'react-hook-form';
+import { motion } from 'framer-motion';
 
 const cn = classNames.bind(styles);
+
+interface Iform {
+  keyword: string;
+}
 
 const Header = () => {
   // const user = useRecoilValue(userState)
   const user = false; // 임시로 해놓음
+
+  const [menuToggle, setMenuToggle] = useState<boolean>(false);
+  const onClickMenu = () => {
+    setMenuToggle(false);
+  };
+
+  const { register, handleSubmit, reset } = useForm<Iform>();
+  const navigate = useNavigate();
+  const onValid = (data: Iform) => {
+    navigate(`/search?keyword=${data.keyword}`);
+    setMenuToggle(false);
+    reset();
+  };
 
   return (
     <header className={cn('header')}>
@@ -22,6 +42,7 @@ const Header = () => {
           <button
             className={cn('toggleBtn')}
             aria-label="JOYRIDE 메뉴 보기 버튼"
+            onClick={() => setMenuToggle(!menuToggle)}
           >
             <GiHamburgerMenu />
           </button>
@@ -30,6 +51,7 @@ const Header = () => {
             to="/"
             className={cn('home')}
             aria-label="메인 페이지 링크 버튼"
+            onClick={onClickMenu}
           >
             <img className={cn('logo')} src={logo} alt="로고" />
           </Link>
@@ -39,6 +61,7 @@ const Header = () => {
               to="/mypage"
               className={cn('mypage')}
               aria-label="마이 페이지 링크 버튼"
+              onClick={onClickMenu}
             >
               {/* TODO: 백엔드에서 유저 사진 받아오기 */}
               <FaRegUserCircle />
@@ -53,6 +76,41 @@ const Header = () => {
             </Link>
           )}
         </div>
+        {menuToggle && (
+          <motion.div
+            className={cn('wrapper')}
+            animate={{ y: [-5, 0], opacity: [0, 1] }}
+            transition={{ ease: 'easeOut', duration: 0.5 }}
+          >
+            <Link
+              to="/roads"
+              className={cn('roads', 'menus')}
+              aria-label="자전거 코스 링크 버튼"
+              onClick={onClickMenu}
+            >
+              자전거 코스
+            </Link>
+            <Link
+              to="/meetups"
+              className={cn('meetups', 'menus')}
+              aria-label="자전거 모임 링크 버튼"
+              onClick={onClickMenu}
+            >
+              자전거 모임
+            </Link>
+            <form
+              className={cn('form', 'menus')}
+              onSubmit={handleSubmit(onValid)}
+            >
+              <input
+                {...register('keyword', { required: true })}
+                className={cn('input')}
+                placeholder="궁금한 내용을 검색해 보세요!"
+              />
+              <AiOutlineSearch className={cn('icon')} />
+            </form>
+          </motion.div>
+        )}
       </Container>
     </header>
   );
