477cffdee4761f49f603b9b2110e412eb9fc0de5
Jihyun Kim <ang21icjh@gmail.com>
2022-07-22 22:44:26 +0900

    feat: 로그인/회원가입 시 Footer 숨김

--- client/src/components/layout/Footer/index.tsx
+++ client/src/components/layout/Footer/index.tsx
@@ -1,3 +1,4 @@
+import { useMatch } from 'react-router-dom';
 import Container from 'components/common/Container';
 import logo_green from 'assets/images/logo_green.svg';
 import styles from './Footer.module.scss';
@@ -5,34 +6,40 @@ import classNames from 'classnames/bind';
 
 const cn = classNames.bind(styles);
 
-const Footer = () => (
-  <footer className={cn('footer')}>
-    <Container>
-      <img className={cn('logo')} src={logo_green} alt="로고" />
+const Footer = () => {
+  const isOnLogin = useMatch('login');
+  const isOnSignup = useMatch('signup');
+  if (isOnLogin || isOnSignup) return null;
 
-      <h3 className={cn('brand')}>JOYRIDE 조이라이드</h3>
+  return (
+    <footer className={cn('footer')}>
+      <Container>
+        <img className={cn('logo')} src={logo_green} alt="로고" />
 
-      <div className={cn('links')}>
-        <a
-          className={cn('link')}
-          href="
+        <h3 className={cn('brand')}>JOYRIDE 조이라이드</h3>
+
+        <div className={cn('links')}>
+          <a
+            className={cn('link')}
+            href="
         https://eight-profit-b8e.notion.site/JoyRide-387ba8a71ce34dadb1ea48c15ee79533"
-          target="_blank"
-          rel="noreferrer"
-        >
-          About Us
-        </a>
-        <a
-          className={cn('link')}
-          href="https://github.com/JOY-RIDE/JOYRIDE"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub Repository
-        </a>
-      </div>
-    </Container>
-  </footer>
-);
+            target="_blank"
+            rel="noreferrer"
+          >
+            About Us
+          </a>
+          <a
+            className={cn('link')}
+            href="https://github.com/JOY-RIDE/JOYRIDE"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub Repository
+          </a>
+        </div>
+      </Container>
+    </footer>
+  );
+};
 
 export default Footer;
