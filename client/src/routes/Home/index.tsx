11dc5382c1f4736ac6dba12ebffe677b21070605
Suehyun Kim <sue555@naver.com>
2022-07-29 16:36:55 +0900

    design: main 화면 layout 구조

--- client/src/routes/Home/index.tsx
+++ client/src/routes/Home/index.tsx
@@ -1,5 +1,31 @@
+import { Link } from 'react-router-dom';
 import styles from './Home.module.scss';
+import classNames from 'classnames/bind';
+import MeetUp from 'components/home/MeetUp';
+import { AiOutlineRight } from 'react-icons/ai';
 
-const Home = () => <h1>Home</h1>;
+const cn = classNames.bind(styles);
+
+const Home = () => (
+  <section className={styles.home}>
+    <div className={cn('content')}>
+      <div className={cn('top')}>
+        <span className={cn('title')}>인기 있는 코스</span>
+        <Link to="/roads" className={cn('to-list')}>
+          더보기 <AiOutlineRight />
+        </Link>
+      </div>
+    </div>
+    <div className={cn('content')}>
+      <div className={cn('top')}>
+        <span className={cn('title')}>모집 중인 모임</span>
+        <Link to="/meetups" className={cn('to-list')}>
+          더보기 <AiOutlineRight />
+        </Link>
+      </div>
+      <MeetUp />
+    </div>
+  </section>
+);
 
 export default Home;
