9dac349fcf31a0257b1966c0a6c9a99e2bf0a8d9
Suehyun Kim <sue555@naver.com>
2022-08-07 04:35:16 +0900

    feat: 자전거 코스 목록 나열

--- client/src/routes/Road/index.tsx
+++ client/src/routes/Road/index.tsx
@@ -1,6 +1,58 @@
-import styles from './Road.module.css';
+import {
+  useLocation,
+  useParams,
+  useMatch,
+  Outlet,
+  Link,
+} from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { fetchCourseInfo } from '../../api';
+import styles from './Road.module.scss';
 import classNames from 'classnames/bind';
+import PageTitle from 'components/common/PageTitle';
+import _ from 'lodash';
 
-const Road = () => <h1>Road</h1>;
+interface RouteState {
+  state: {
+    name: string;
+  };
+}
+
+interface IRoad {
+  brdDiv: string;
+  createdtime: number;
+  crsContents: string;
+  crsCycle: string;
+  crsDstnc: number;
+  crsIdx: string;
+  crsKorNm: string;
+  crsLevel: number;
+  crsSummary: string;
+  crsTotlRqrmHour: number;
+  crsTourInfo: string;
+  gpxpath: string;
+  modifiedtime: number;
+  routeIdx: string;
+  sigun: string;
+  travelerinfo: number;
+}
+
+const Road = () => {
+  const { courseNm } = useParams<{ courseNm: string }>();
+  const { state } = useLocation() as RouteState;
+
+  const { data } = useQuery<IRoad>(['info', courseNm], () =>
+    fetchCourseInfo(courseNm)
+  );
+
+  return (
+    <section className={styles.road}>
+      <PageTitle size="md">
+        {state?.name}
+        {courseNm}
+      </PageTitle>
+    </section>
+  );
+};
 
 export default Road;
--- client/src/routes/Roads/index.tsx
+++ client/src/routes/Roads/index.tsx
@@ -5,6 +5,8 @@ import styles from './Roads.module.scss';
 import classNames from 'classnames/bind';
 import PageTitle from 'components/common/PageTitle';
 import Loading from 'components/common/Loading';
+import { AiFillHeart } from 'react-icons/ai';
+import _ from 'lodash';
 
 const cn = classNames.bind(styles);
 
@@ -29,11 +31,64 @@ interface IRoad {
 
 const Roads = () => {
   const { isLoading, data } = useQuery<IRoad[]>('allCourses', fetchCourses);
+  const RoadsData = _.uniqBy(data, 'crsIdx');
   //   console.log(data);
   return (
-    <div>
-      {isLoading ? <Loading /> : <PageTitle size="md">자전거 코스</PageTitle>}
-    </div>
+    <section className={styles.roads}>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className={cn('container')}>
+          <PageTitle size="md">자전거 코스</PageTitle>
+          <ul className={cn('contents')}>
+            {RoadsData?.map(road => (
+              <li className={cn('content')} key={road.crsKorNm}>
+                <Link to={`${road.crsKorNm}`} state={{ name: road.crsKorNm }}>
+                  <p className={cn('title')}>
+                    <span className={cn('sigun')}>{road.sigun}</span>{' '}
+                    <span className={cn('name')}>{road.crsKorNm}</span>
+                  </p>
+                  <p className={cn('info')}>
+                    <span className={cn('dstnc')}>{road.crsDstnc}km </span>
+                    <span className={cn('hour')}>
+                      {road.crsTotlRqrmHour < 60
+                        ? `${road.crsTotlRqrmHour}분`
+                        : road.crsTotlRqrmHour % 60 == 0
+                        ? `${Math.floor(road.crsTotlRqrmHour / 60)}시간`
+                        : `${Math.floor(road.crsTotlRqrmHour / 60)}시간 ${
+                            road.crsTotlRqrmHour % 60
+                          }분`}
+                    </span>
+                  </p>
+                  <p className={cn('info2')}>
+                    <span className={cn('level')}>
+                      <span className={cn('type')}>난이도</span>{' '}
+                      <span className={cn('value')}>
+                        {road.crsLevel == 1
+                          ? '하'
+                          : road.crsLevel == 2
+                          ? '중'
+                          : '상'}
+                      </span>
+                    </span>
+                    ·
+                    <span className={cn('rate')}>
+                      <span className={cn('type')}>평점</span>{' '}
+                      <span className={cn('value')}>4.5</span>
+                    </span>
+                    ·
+                    <span className={cn('likes')}>
+                      <span className={cn('type')}>♥</span>{' '}
+                      <span className={cn('value')}>12</span>
+                    </span>
+                  </p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </section>
   );
 };
 
