a195d1c67b015066c445848e729fbae56f31f564
wonseok <tkddls23@konkuk.ac.kr>
2022-08-09 19:50:57 +0900

    feat: social_signup&signin_refreshToken

--- src/main/java/com/joyride/ms/src/auth/PrincipalOAuth2UserService.java
+++ src/main/java/com/joyride/ms/src/auth/PrincipalOAuth2UserService.java
@@ -1,32 +1,41 @@
 package com.joyride.ms.src.auth;
 
+import com.joyride.ms.src.auth.dto.GetOauth2UserRes;
+import com.joyride.ms.src.auth.dto.PostSignupOauth2Req;
 import com.joyride.ms.src.auth.model.PrincipalDetails;
+import com.joyride.ms.src.user.UserDao;
 import com.joyride.ms.src.user.UserProvider;
 import com.joyride.ms.src.user.UserService;
 import com.joyride.ms.src.user.model.User;
 import com.joyride.ms.util.BaseException;
+import com.joyride.ms.util.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import static com.joyride.ms.util.BaseResponseStatus.SUCCESS;
+
 @Slf4j
 @Service
 public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserProvider userProvider;
+    private final UserService userService;
+
+
 
     @Autowired
-    public PrincipalOAuth2UserService(UserProvider userProvider) {
+    public PrincipalOAuth2UserService(UserProvider userProvider, UserService userService) {
         this.userProvider = userProvider;
+        this.userService = userService;
     }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        log.info("test");
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
         String name = (String) oAuth2User.getAttributes().get("name");
@@ -35,6 +44,8 @@ public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
         String provider_id = (String) oAuth2User.getAttributes().get("sub");
 
         User user = null;
+        User newUser = null;
+
         // todo: 회원가입된 소셜계정 validate
         try {
             if (userProvider.checkEmail(email, provider) == 1)
@@ -47,7 +58,15 @@ public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
             user = new User(name,email,"", "",null,"","", provider, provider_id);
             // 여기서 회원 가입 실제로 하지 않고, 가능 여부 파악 후 클라이언트에서 /auth/signup/oauth2 api 호출하여 실제 회원가입함
 
-            return new PrincipalDetails(user, oAuth2User.getAttributes(), true);
+            PostSignupOauth2Req postSignupOauth2Req = new PostSignupOauth2Req(email,provider,provider_id,true);
+
+            try{
+                userService.createOauth2User(postSignupOauth2Req);
+                newUser = userProvider.retrieveByEmail(email, provider);
+            }catch (BaseException e) {
+                throw new RuntimeException(e);
+            }
+            return new PrincipalDetails(newUser, oAuth2User.getAttributes(), true);
         } else {
             log.info("구글 로그인 기록이 있습니다. 로그인을 진행합니다.");
             return new PrincipalDetails(user, oAuth2User.getAttributes(), false);
--- src/main/java/com/joyride/ms/src/config/SecurityConfig.java
+++ src/main/java/com/joyride/ms/src/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.joyride.ms.src.jwt.handler.CustomAccessDeniedHandler;
 import com.joyride.ms.src.jwt.handler.CustomAuthenticationEntryPointHandler;
 import com.joyride.ms.src.jwt.handler.OAuth2SuccessHandler;
 import com.joyride.ms.src.user.UserProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,19 +17,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig {
 
     private final PrincipalOAuth2UserService principalOAuth2UserService;
@@ -91,9 +86,5 @@ public class SecurityConfig {
                 .successHandler(new OAuth2SuccessHandler(jwtTokenProvider, authService));
 
         return http.build();
-    }
-        @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
