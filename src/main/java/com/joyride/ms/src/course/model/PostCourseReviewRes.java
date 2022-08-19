e87c615095e95b5acd1b502c272e34d76390a612
Sangwoo Shin <sangwoonoel@naver.com>
2022-08-20 00:28:51 +0900

    feat: 코스리뷰 엔티티 작성

---
+++ src/main/java/com/joyride/ms/src/course/model/PostCourseReviewRes.java
@@ -0,0 +1,14 @@
+package com.joyride.ms.src.course.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostCourseReviewRes {
+
+    private int id;
+
+    private String message;
+}
+
