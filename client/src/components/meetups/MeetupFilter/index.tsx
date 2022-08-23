f0203bee67b0338a176ef926786ae4a25208e2ae
Jihyun Kim <ang21icjh@gmail.com>
2022-08-24 03:52:24 +0900

    feat: Meetup 코스 난이도/자전거 종류 필터 선택

--- client/src/components/meetups/MeetupFilter/index.tsx
+++ client/src/components/meetups/MeetupFilter/index.tsx
@@ -4,26 +4,49 @@ import CheckBox from 'components/common/CheckBox';
 import styles from './MeetupFilter.module.scss';
 // import styles from '../../roads/Filter/Filter.module.scss';
 import classNames from 'classnames/bind';
-import { LOCATIONS } from 'utils/constants';
+import {
+  BICYCLE_TYPES,
+  LOCATIONS,
+  MEETUP_PATH_DIFFICULTIES,
+} from 'utils/constants';
 import OptionChip from 'components/common/OptionChip';
-import { MeetupFilterOptionName, MeetupFilterState } from 'types/meetup';
+import { MeetupBicycleType, MeetupFilterState } from 'types/meetup';
 import { omit } from 'lodash';
 import { useRecoilState, useResetRecoilState } from 'recoil';
 import { meetupFilterState } from 'states/meetup';
 import { FilterDispatchAction, FilterDispatchPayload } from 'types/common';
+import { stringifyDifficulty } from 'utils/stringify';
 
 const cn = classNames.bind(styles);
 
+interface OptionData {
+  value: number | string;
+  content: string;
+}
+
+/** - 단일 선택 옵션 형태: { 옵션명: {value: 값, content: 한글} }
+ * - 다중 선택 옵션 형태: { 옵션명: [{value: 값, content: 한글}] }
+ */
 function meetupFilterDispatch(
   state: MeetupFilterState,
   action: FilterDispatchAction,
   { name, value, content }: FilterDispatchPayload
 ) {
+  const data = { value, content };
   switch (action) {
     case 'CHOOSE':
       switch (name) {
         case 'location':
-          return { ...state, [name]: { value, content } };
+        case 'pathDifficulty':
+          return { ...state, [name]: data };
+
+        case 'bicycleType': {
+          const oldDataArray = state[name];
+          return oldDataArray
+            ? { ...state, [name]: oldDataArray.concat(data) }
+            : { ...state, [name]: [data] };
+        }
+
         default:
           throw new Error();
       }
@@ -31,7 +54,21 @@ function meetupFilterDispatch(
     case 'REMOVE':
       switch (name) {
         case 'location':
+        case 'pathDifficulty':
           return omit(state, [name]);
+
+        case 'bicycleType': {
+          const oldDataArray = state[name];
+          return oldDataArray.length > 1
+            ? {
+                ...state,
+                [name]: oldDataArray.filter(
+                  (data: OptionData) => data.value !== value
+                ),
+              }
+            : omit(state, [name]);
+        }
+
         default:
           throw new Error();
       }
@@ -89,14 +126,60 @@ const MeetupFilter = () => {
               ))}
             </ul>
           </li>
+
           <li className={cn('row')}>
             <label className={cn('label')}>코스 난이도</label>
-            <ul className={cn('options')}>공간</ul>
+            <ul className={cn('options')}>
+              <OptionChip
+                name="pathDifficulty"
+                value="전체"
+                content="전체"
+                isChosen={!filter.pathDifficulty}
+                onTextClick={clearOption}
+              />
+              {MEETUP_PATH_DIFFICULTIES.map((difficulty, index) => (
+                <OptionChip
+                  key={index}
+                  name="pathDifficulty"
+                  value={difficulty}
+                  content={stringifyDifficulty(difficulty)}
+                  isChosen={difficulty === filter.pathDifficulty?.value}
+                  onTextClick={chooseOption}
+                  onXClick={removeOption}
+                />
+              ))}
+            </ul>
           </li>
+
           <li className={cn('row')}>
             <label className={cn('label')}>자전거 종류</label>
-            <ul className={cn('options')}>공간</ul>
+            <ul className={cn('options')}>
+              <OptionChip
+                name="bicycleType"
+                value="전체"
+                content="전체"
+                isChosen={!filter.bicycleType}
+                onTextClick={clearOption}
+              />
+              {BICYCLE_TYPES.map((type, index) => (
+                <OptionChip
+                  key={index}
+                  name="bicycleType"
+                  value={type}
+                  content={type}
+                  isChosen={
+                    filter.bicycleType &&
+                    filter.bicycleType.some(
+                      (data: OptionData) => data.value === type
+                    )
+                  }
+                  onTextClick={chooseOption}
+                  onXClick={removeOption}
+                />
+              ))}
+            </ul>
           </li>
+
           <li className={cn('row')}>
             <label className={cn('label')}>라이딩 실력</label>
             <ul className={cn('options')}>공간</ul>
@@ -127,15 +210,44 @@ const MeetupFilter = () => {
         <div className={cn('choices-container')}>
           <ul className={cn('choices')}>
             {filter.location && (
-              <OptionChip
-                name="location"
-                value={filter.location.value}
-                content={filter.location.content}
-                isChosen
-                onTextClick={chooseOption}
-                onXClick={removeOption}
-              />
+              <li>
+                <OptionChip
+                  name="location"
+                  value={filter.location.value}
+                  content={filter.location.content}
+                  isChosen
+                  onTextClick={chooseOption}
+                  onXClick={removeOption}
+                />
+              </li>
             )}
+
+            {filter.pathDifficulty && (
+              <li>
+                <OptionChip
+                  name="pathDifficulty"
+                  value={filter.pathDifficulty.value}
+                  content={filter.pathDifficulty.content}
+                  isChosen
+                  onTextClick={chooseOption}
+                  onXClick={removeOption}
+                />
+              </li>
+            )}
+
+            {filter.bicycleType &&
+              filter.bicycleType.map(({ value, content }: OptionData) => (
+                <li key={value}>
+                  <OptionChip
+                    name="bicycleType"
+                    value={value}
+                    content={content}
+                    isChosen
+                    onTextClick={chooseOption}
+                    onXClick={removeOption}
+                  />
+                </li>
+              ))}
           </ul>
         </div>
 
--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,19 +1,13 @@
+import { MeetupPathDifficulty } from 'types/meetup';
 import { BicycleType, Location } from 'types/common';
 
 export const MAIN_COLOR = '#22b573';
-
 export const REGEX = {
   email:
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   password: /[0-9]/,
 };
 
-export const BICYCLE_TYPES: BicycleType[] = [
-  '따릉이',
-  'MTB',
-  '로드바이크',
-  '하이브리드',
-];
 export const LOCATIONS: Location[] = [
   '서울',
   '인천',
@@ -26,4 +20,12 @@ export const LOCATIONS: Location[] = [
   '경북',
   '경남',
   '제주',
+];
+
+export const MEETUP_PATH_DIFFICULTIES: MeetupPathDifficulty[] = [1, 2, 3];
+export const BICYCLE_TYPES: BicycleType[] = [
+  '따릉이',
+  'MTB',
+  '로드바이크',
+  '하이브리드',
 ];
