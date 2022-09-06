81bd6b4ec17be6046246bc75d0886ce976df94bc
Sangwoo Shin <sangwoonoel@naver.com>
2022-09-06 21:30:16 +0900

    feat: GetCourseNameList Class(#104)

---
+++ src/main/java/com/joyride/ms/src/course/model/GetCourseNameListRes.java
@@ -0,0 +1,19 @@
+package com.joyride.ms.src.course.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GetCourseNameListRes /*implements Comparable<GetCourseNameListRes>*/ {
+    private String title;
+
+    /*
+    @Override
+    public int compareTo(GetCourseNameListRes o) {
+
+    }
+
+     */
+}
