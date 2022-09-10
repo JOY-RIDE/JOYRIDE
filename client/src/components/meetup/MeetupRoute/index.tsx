c1c50a6feaef02b2b06929bf489d423b60f9e589
Jihyun Kim <ang21icjh@gmail.com>
2022-09-10 19:54:45 +0900

    feat: 모임 디테일 -> 코스 디테일 페이지 연결

--- client/src/components/meetup/MeetupRoute/index.tsx
+++ client/src/components/meetup/MeetupRoute/index.tsx
@@ -2,8 +2,10 @@ import { MeetupCourseName, MeetupPath } from 'types/meetup';
 import styles from './MeetupRoute.module.scss';
 import classNames from 'classnames/bind';
 import { BsArrowRight } from 'react-icons/bs';
+import { TbLayersLinked } from 'react-icons/tb';
 import Chip from 'components/common/Chip';
 import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const cn = classNames.bind(styles);
 
@@ -14,7 +16,14 @@ interface MeetupRouteProps {
 
 const MeetupRoute = ({ courseName, path }: MeetupRouteProps) => (
   <div className={cn('container', { long: courseName })}>
-    {courseName && <span className={cn('course')}>{courseName}</span>}
+    {courseName && (
+      <Link to={`/roads/${courseName}`}>
+        <span className={cn('course')}>
+          {courseName}
+          <TbLayersLinked />
+        </span>
+      </Link>
+    )}
     <ul className={cn('path')}>
       {path.map((stop, index) => (
         <Fragment key={index}>
