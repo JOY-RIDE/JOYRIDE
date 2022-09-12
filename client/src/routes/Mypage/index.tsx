7d7322aed0144010140c1fdb3eab1f8c9c393f64
Jihyun Kim <ang21icjh@gmail.com>
2022-09-12 15:35:35 +0900

    design: 참가/북마크한 모임 리스트

--- client/src/routes/Mypage/index.tsx
+++ client/src/routes/Mypage/index.tsx
@@ -2,28 +2,23 @@ import Profile from 'components/Mypage/Profile';
 import styles from './Mypage.module.scss';
 import classNames from 'classnames/bind';
 import MyMeetupsPreview from 'components/Mypage/MyMeetupsPreview';
+import BookmarkedMeetupsPreview from 'components/Mypage/BookmarkedMeetupsPreview';
+import JoinedMeetupsPreview from 'components/Mypage/JoinedMeetupsPreview';
+import SectionTitle from 'components/Mypage/SectionTitle';
 
 const cn = classNames.bind(styles);
 
 const MyPage = () => (
   <div className={cn('container')}>
     <Profile />
-    <section className={cn('liked-courses')}>
-      <h2 className={cn('title')}>
-        좋아요 표시한 코스<span></span>
-      </h2>
+    <section className={cn('wrapper')}>
+      <header className={cn('header')}>
+        <SectionTitle title="좋아요 표시한 코스" />
+      </header>
     </section>
     <MyMeetupsPreview />
-    <section className={cn('joined-meetups')}>
-      <h2 className={cn('title')}>
-        참가한 모임<span></span>
-      </h2>
-    </section>
-    <section className={cn('bookmarked-meetups')}>
-      <h2 className={cn('title')}>
-        북마크한 모임<span></span>
-      </h2>
-    </section>
+    <JoinedMeetupsPreview />
+    <BookmarkedMeetupsPreview />
   </div>
 );
 
