0d0d5d909d798b5159be8707bc185e1b39e194ed
Suehyun Kim <sue555@naver.com>
2022-09-15 04:23:49 +0900

    feat: 좋아요 취소

--- client/src/routes/Roads/index.tsx
+++ client/src/routes/Roads/index.tsx
@@ -53,7 +53,7 @@ const Roads = () => {
     // @ts-ignore
     COURSE_FILTERS_DISPATCHES
   );
-  console.log(boardFilters);
+  //   console.log(boardFilters);
 
   const filterLocation = boardFilters?.location?.value;
   const filterLevel = boardFilters?.pathDifficulty?.value;
