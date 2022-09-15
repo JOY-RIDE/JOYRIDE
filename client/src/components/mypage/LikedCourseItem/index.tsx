ea324dbf8fe5f3b2cfc99e57588ef7d479145ff8
Jihyun Kim <ang21icjh@gmail.com>
2022-09-15 23:18:55 +0900

    feat: 마이페이지 코스 좋아요 취소

--- client/src/components/mypage/LikedCourseItem/index.tsx
+++ client/src/components/mypage/LikedCourseItem/index.tsx
@@ -1,25 +1,24 @@
 import styles from './LikedCourseItem.module.scss';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useSetRecoilState } from 'recoil';
+import { useQueryClient } from '@tanstack/react-query';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { toastMessageState } from 'states/common';
 import { courseAPI } from 'apis/courseAPI';
 import { HiHeart } from 'react-icons/hi';
+import { userIdState } from 'states/auth';
 
 const cn = classNames.bind(styles);
 
 const LikedCourseItem = ({ crsKorNm, image, crsContents }: any) => {
+  const userId = useRecoilValue(userIdState) as number;
   const showToastMessage = useSetRecoilState(toastMessageState);
   const queryClient = useQueryClient();
-  // const { mutate } = useMutation(courseAPI.cancelCourseLike, {
-  //   onSuccess: () => {
-  //     showToastMessage('좋아요를 취소했습니다.');
-  //     queryClient.invalidateQueries(['serverInfo', 'like']);
-  //   },
-  //   onError: () => showToastMessage('좋아요 취소 중 문제가 발생했습니다.'),
-  // });
-  // const cancelLike = () => mutate(id);
+  const cancelCourseLike = async () => {
+    await courseAPI.cancelCourseLike(crsKorNm, userId);
+    showToastMessage('좋아요를 취소했습니다.');
+    queryClient.invalidateQueries(['serverInfo', 'like']);
+  };
 
   return (
     <li className={cn('container')}>
@@ -29,7 +28,11 @@ const LikedCourseItem = ({ crsKorNm, image, crsContents }: any) => {
       </Link>
 
       <img className={cn('img')} src={image} alt={crsKorNm} />
-      <button className={cn('cancel-btn')} aria-label="코스 좋아요 취소 버튼">
+      <button
+        className={cn('cancel-btn')}
+        aria-label="코스 좋아요 취소 버튼"
+        onClick={cancelCourseLike}
+      >
         <HiHeart />
       </button>
     </li>
