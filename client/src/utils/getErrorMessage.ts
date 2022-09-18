c6c318df262724241416b1bfdfde05d7c9ca5c0b
Suehyun Kim <sue555@naver.com>
2022-09-18 16:40:46 +0900

    feat: 유저 정보 수정 api

--- client/src/utils/getErrorMessage.ts
+++ client/src/utils/getErrorMessage.ts
@@ -214,7 +214,7 @@ export function getMeetupCreationFormFieldErrorMessage(
 type ModifierFormField =
   | 'image'
   | 'nickname'
-  | 'ridingSkill'
+  | 'Bicyclecareer'
   | 'bicycleType'
   | 'introduce';
 export function getModifierFormFieldErrorMessage(
@@ -244,7 +244,7 @@ export function getModifierFormFieldErrorMessage(
       }
     }
 
-    case 'ridingSkill': {
+    case 'Bicyclecareer': {
       switch (errorType) {
         default:
           throw new Error();
