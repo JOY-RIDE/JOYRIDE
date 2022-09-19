5bda34a0bbc9b171bbec14c166cf5dde155b7766
Suehyun Kim <sue555@naver.com>
2022-09-19 22:52:29 +0900

    chore: 리뷰 작성 버튼 사이즈 조정

--- client/src/components/road/ReviewWriter/index.tsx
+++ client/src/components/road/ReviewWriter/index.tsx
@@ -44,7 +44,7 @@ const ReviewWriter = () => {
         <Button
           type="button"
           color="whiteMain"
-          size="md"
+          size="lg"
           content="후기 쓰기"
           onClick={handlePopupOpen}
         ></Button>
